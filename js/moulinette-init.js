231a3fbcc55dc071d70fe7e86ce4d7e210d37fec
Sven Werlen <sven.werlen@pm.me>
2022-08-31 23:48:31 -0400

    Improvements to drop zone

--- js/moulinette-init.js
+++ js/moulinette-init.js
@@ -3,16 +3,22 @@
 const moulinette = document.getElementById("moulinette")
 if(!moulinette) {
   $.ajax({
-      url: chrome.runtime.getURL("html/moulinette.html"),
+      url: chrome.runtime.getURL("html/moulinette-panel.html"),
       cache: false
   }).done(function(data) {
     const panel = document.createElement('div');
     panel.id = "moulinette-panel"
     document.body.appendChild(panel);
     $(panel).html(data);
+  });
+  $.ajax({
+      url: chrome.runtime.getURL("html/moulinette-drop.html"),
+      cache: false
+  }).done(function(data) {
     const dropzone = document.createElement('div');
     dropzone.id = "moulinette-drop"
     document.body.appendChild(dropzone);
+    $(dropzone).html(data);
   });
 }
 
