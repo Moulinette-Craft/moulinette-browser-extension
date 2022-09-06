cc4850fdfd13c33c7b42f40807696d0629866790
Sven Werlen <sven.werlen@pm.me>
2022-09-05 22:38:43 -0400

    Temporary fix for chrome

--- moulinette.js
+++ moulinette.js
@@ -1,24 +1,34 @@
-"use strict";
 
 async function toggleMoulinettePanel() {
 
-  // retrieve currently active tab
-  const tabs = await (browser ? browser : chrome).tabs.query({ currentWindow: true, active: true })
-  if(tabs && tabs.length == 1) {
-    (browser ? browser : chrome).tabs.sendMessage(tabs[0].id, {action: "togglePanel"});
+  /** Default implementation **/
+  if(typeof browser !== "undefined") {
+    // retrieve currently active tab
+    const tabs = await browser.tabs.query({ currentWindow: true, active: true, lastFocusedWindow: true })
+    if(tabs && tabs.length == 1) {
+      browser.tabs.sendMessage(tabs[0].id, {action: "togglePanel"});
+    }
+  }
+  // Chrome implementation
+  else {
+    chrome.tabs.query({ active: true, lastFocusedWindow: true }, ([tab]) => {
+      chrome.tabs.sendMessage(tab.id, {action: "togglePanel"});
+    });
   }
 }
 
 // default (firefox and others)
-if(browser) {
-  browser.browserAction.onClicked.addListener(toggleMoulinettePanel);
-  browser.commands.onCommand.addListener((command) => {
-    if (command === "toggle-panel") {
-      toggleMoulinettePanel()
-    }
-  });
-}
+//if(typeof browser !== "undefined") {
+(typeof browser !== "undefined" ? browser : chrome).browserAction.onClicked.addListener(toggleMoulinettePanel);
+(typeof browser !== "undefined" ? browser : chrome).commands.onCommand.addListener((command) => {
+  if (command === "toggle-panel") {
+    toggleMoulinettePanel()
+  }
+});
+
+//}
 // chrome specific (manifest v3)
+/*
 else {
   chrome.action.onClicked.addListener((tab) => {
     chrome.scripting.executeScript({
@@ -29,4 +39,5 @@ else {
     });
   });
 }
+*/
 
